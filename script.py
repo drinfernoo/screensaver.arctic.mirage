829e363bce21fa23b7b51abbfff2be8b79456fde
drinfernoo <drinfernoo@yahoo.com>
2020-02-12 19:02:36 -0800

    :tada: - Initial commit

---
+++ script.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import xbmc
+import xbmcaddon
+
+import os
+import sys
+
+_addon = xbmcaddon.Addon()
+_id = _addon.getAddonInfo('id')
+
+def get_params():
+    xbmc.log(sys.argv, level=xbmc.LOGNOTICE)
+    for arg in sys.argv:
+        if arg == 'script.py':
+            pass
+        elif '=' in arg:
+            arg_split = arg.split('=', 1)
+            if arg_split[0] and arg_split[1]:
+                return arg_split
+
+
+if __name__ == '__main__':
+    if get_params() == ['mode', 'choose']:
+        call = ('RunScript(script.skinshortcuts,'
+                'type=widgets'
+                '&showNone=False'
+                '&skinWidgetName={0}.name'
+                '&skinWidgetPath={0}.path)'.format(_id))
+        xbmc.executebuiltin(call, wait=True)
+        
+        name = xbmc.getInfoLabel('Skin.String({}}.name'.format(_id))
+        _addon.setSettingString('{}}.name'.format(_id), name)
